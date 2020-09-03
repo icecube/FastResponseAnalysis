33978a8f19c8779432334132e4ce0a156310f3bf
Alex Pizzuto <pizzuto@wisc.edu>
2020-09-03 15:52:06 -0500

    script to make all zoomed and all sky maps

---
+++ trunk/make_all_skymaps.py
@@ -0,0 +1,27 @@
+import sys
+sys.path.append('/data/user/apizzuto/fast_response_skylab/fast-response/trunk/time_integrated_scripts/')
+from steady_sensitivity_fits import *
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+
+output_path = '/data/user/apizzuto/fast_response_skylab/alert_event_followup/alert_skymaps/'
+
+skymap_files = glob('/data/ana/realtime/alert_catalog_v2/fits_files/Run*.fits.gz')
+l_ind = skymap_files[0].find("Run")
+r_ind = skymap_files[0].find("_nside")
+
+for ind in range(len(skymap_files)):
+    title = skymap_files[ind][l_ind:r_ind].replace('_', '_Event_').replace('Run', 'Run_')
+    plot_zoom(ind, LLH=True)
+    zoom_str = 'zoom_LLH'
+    plt.savefig(output_path + '{}_{}.png'.format(title, zoom_str), bbox_inches='tight')
+
+    plot_zoom(ind, LLH=False)
+    zoom_str = 'probs'
+    plt.savefig(output_path + '{}_{}.png'.format(title, zoom_str), bbox_inches='tight')
+
+    plot_skymap(ind)
+    zoom_str = 'allsky'
+    plt.savefig(output_path + '{}_{}.png'.format(title, zoom_str), bbox_inches='tight')
+    
