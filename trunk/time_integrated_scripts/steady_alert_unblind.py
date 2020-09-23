cb364402e68750f73f2b8c0e784f61b4039ab56f
Alex Pizzuto <pizzuto@wisc.edu>
2020-09-23 15:33:35 -0500

    option to look at local ts map with cut for output file size

--- trunk/time_integrated_scripts/steady_alert_unblind.py
+++ trunk/time_integrated_scripts/steady_alert_unblind.py
@@ -30,6 +30,8 @@ parser.add_argument('--verbose', action='store_true', default=False,
 parser.add_argument('--rng', type=int, default=1, help="Random number seed")
 parser.add_argument('--smear', default=False, action='store_true',
                     help='Include systematics by smearing norm. prob.')
+parser.add_argument('--local_skymap', default=False, action='store_true',
+                    help='Instead of returning best fit, return whole map')
 args = parser.parse_args()
 #######################################################################################
 
@@ -38,7 +40,10 @@ seed = args.rng
 verbose = args.verbose
 
 smear_str = 'smeared/' if args.smear else 'norm_prob/'
-outfile = '/data/user/apizzuto/fast_response_skylab/alert_event_followup/analysis_trials/results/{}index_{}_steady_seed_{}.pkl'.format(smear_str, index, seed)
+if not args.local_skymap:
+    outfile = '/data/user/apizzuto/fast_response_skylab/alert_event_followup/analysis_trials/results/{}index_{}_steady_seed_{}.pkl'.format(smear_str, index, seed)
+else:
+    outfile = '/data/user/apizzuto/fast_response_skylab/alert_event_followup/analysis_trials/results/{}index_{}_steady_ts_map_seed_{}.pkl'.format(smear_str, index, seed)
 
 t0 = time.time()
 
@@ -49,11 +54,12 @@ multillh, spatial_prior = config(index, gamma = 2.0, seed = seed, scramble = Fal
 t1 = time.time()
 if verbose:
     print('{:.2f} seconds to Initialize Likelihoods'.format(t1 - t0))
-    print ("\nRunning background only trials ...")
+    print ("\nRunning fit on real data ...")
 
 allspots = None
 ii = 1
-for results, hotspots in multillh.do_allsky_trials(n_iter= 2, 
+n_iter = 2 if not args.local_skymap else 1
+for results, hotspots in multillh.do_allsky_trials(n_iter= n_iter, 
                               injector=None,
                               nside=nside, rng_seed = 123*seed + ii,
                               spatial_prior=spatial_prior,
@@ -62,22 +68,28 @@ for results, hotspots in multillh.do_allsky_trials(n_iter= 2,
     if verbose:
         print('Trial Number: {}'.format(ii))
     ii += 1
-    if allspots is None:
-        allspots = {}
-        for k, v in hotspots['spatial_prior_0']['best'].items():
-            allspots[k] = [v]
-        if 'pix' not in allspots.keys():
-            allspots['pix'] = [0]
-        if 'nside' not in allspots.keys():
-            allspots['nside'] = [0]
+    if not args.local_skymap:
+        if allspots is None:
+            allspots = {}
+            for k, v in hotspots['spatial_prior_0']['best'].items():
+                allspots[k] = [v]
+            if 'pix' not in allspots.keys():
+                allspots['pix'] = [0]
+            if 'nside' not in allspots.keys():
+                allspots['nside'] = [0]
+        else:
+            for k, v in hotspots['spatial_prior_0']['best'].items():
+                allspots[k].append(v)
+            if 'pix' not in hotspots['spatial_prior_0']['best'].keys():
+                allspots['pix'].append(0)
+            if 'nside' not in hotspots['spatial_prior_0']['best'].keys():
+                allspots['nside'].append(0)
     else:
-        for k, v in hotspots['spatial_prior_0']['best'].items():
-            allspots[k].append(v)
-        if 'pix' not in hotspots['spatial_prior_0']['best'].keys():
-            allspots['pix'].append(0)
-        if 'nside' not in hotspots['spatial_prior_0']['best'].keys():
-            allspots['nside'].append(0)
+        allspots = results
     #allspots.append(hotspots)
+
+if args.local_skymap:
+    allspots = allspots[allspots['TS'] != 0.]
 
 dt1 = t1 - t0
 dt     = time.time() - t0
