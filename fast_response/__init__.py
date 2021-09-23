cfa43b3ff96b256585f8ddebc26e21b5564983b5
Alex Pizzuto <pizzuto@wisc.edu>
2021-09-23 13:49:11 -0500

    make imports available in init

---
+++ fast_response/__init__.py
@@ -0,0 +1,6 @@
+# __init__.py
+
+from .AlertFollowup import TrackFollowup, CascadeFollowup
+from .GWFollowup import GWFollowup
+from .GRBFollowup import GRBFollowup
+from .ExternalFollowup import ExternalFollowup
