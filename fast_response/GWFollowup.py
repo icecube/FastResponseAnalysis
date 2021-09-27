a89574c34d287b9545d918f368d41e206a1ce96c
Alex Pizzuto <pizzuto@wisc.edu>
2021-09-27 12:07:27 -0500

    Correct dataset name for GW

--- fast_response/GWFollowup.py
+++ fast_response/GWFollowup.py
@@ -1,7 +1,7 @@
 from .FastResponseAnalysis import PriorFollowup
 
 class GWFollowup(PriorFollowup):
-    _dataset = 'GFUOnline_v001_p03'
+    _dataset = 'GFUOnline_v001_p02'
     _fix_index = False
     _index = 2.0
     pass
