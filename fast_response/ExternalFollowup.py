848da88cf0d01292abb80ba6e4bf7702124a20f2
Alex Pizzuto <pizzuto@wisc.edu>
2021-09-27 16:06:53 -0500

    Correct the underscores in dataset

--- fast_response/ExternalFollowup.py
+++ fast_response/ExternalFollowup.py
@@ -1,4 +1,7 @@
-from .FastResponseAnalysis import PointSourceFollowup
+from FastResponseAnalysis import PointSourceFollowup
 
 class ExternalFollowup(PointSourceFollowup):
-    _dataset = "GFUOnline_v001_p02"
+    _dataset = "GFUOnline_v001p02"
+    _fix_index = True
+    _float_index = not _fix_index
+    _index = 2.0
