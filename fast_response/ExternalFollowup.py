24d9f0e8eebe500816e72b296c3a29960bc16d2b
Alex Pizzuto <pizzuto@wisc.edu>
2021-10-26 16:17:22 -0500

    External followup working for point source like followups. Needs more testing

--- fast_response/ExternalFollowup.py
+++ fast_response/ExternalFollowup.py
@@ -1,6 +1,12 @@
-from FastResponseAnalysis import PointSourceFollowup
+from .FastResponseAnalysis import PointSourceFollowup, PriorFollowup
 
 class ExternalFollowup(PointSourceFollowup):
+    _dataset = "GFUOnline_v001p02"
+    _fix_index = True
+    _float_index = not _fix_index
+    _index = 2.0
+
+class ExternalSkymapFollowup(PriorFollowup):
     _dataset = "GFUOnline_v001p02"
     _fix_index = True
     _float_index = not _fix_index
