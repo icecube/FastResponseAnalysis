4bf19f7e348569430a3533aba8c49b1815f6ab7e
jessiethw <thwaites@wisc.edu>
2023-06-15 12:00:42 -0500

    add listener to our alerts

---
+++ fast_response/scripts/test_listen_kafka.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import logging
+from gcn_kafka import Consumer
+from icecube import realtime_tools
+import json
+import argparse
+
+## none of these bools work yet. just listening to the real one
+parser = argparse.ArgumentParser(description='test listener for icecube kafka fra/llama results')
+parser.add_argument('--test_domain', type=bool, default=False,
+                        help='bool to use test.gcn.nasa.gov (default False)')
+parser.add_argument('--test_topic', type=bool, default=True,
+                        help='listen to gcn.notices.icecube.TEST.lvk_nu_track_search')
+args = parser.parse_args()
+
+
+with open('/cvmfs/icecube.opensciencegrid.org/users/jthwaites/tokens/kafka_token.txt') as f:
+    client_id = f.readline().rstrip('\n')
+    client_secret = f.readline().rstrip('\n')
+
+#if args.test_domain:
+#    domain = 'test.gcn.nasa.gov'
+#else:
+domain = 'gcn.nasa.gov'
+
+consumer = Consumer(client_id=client_id,
+                    client_secret=client_secret,
+                    domain=domain)
+
+#if args.test_topic:
+#    topic = 'gcn.notices.icecube.test.lvk_nu_track_search'
+#else:
+topic = 'gcn.notices.icecube.lvk_nu_track_search'
+
+consumer.subscribe([topic])
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.warning("checking for {}, connecting to GCN".format(topic))
+
+while True:
+    for message in consumer.consume(timeout=1):
+        value = message.value()
+        alert_dict = json.loads(value.decode('utf-8'))
+        
+        print(json.dumps(alert_dict, indent=2))
