4ea13c0ac70ef0da7855b441832491fe1b1ffdd5
jessiethw <thwaites@wisc.edu>
2023-11-02 22:18:38 -0500

    move schema link to v3.0.0

--- fast_response/scripts/combine_results_kafka.py
+++ fast_response/scripts/combine_results_kafka.py
@@ -207,7 +207,7 @@ def parse_notice(record, wait_for_llama=False, heartbeat=False):
         return
 
     collected_results = {}
-    collected_results["$schema"]= "https://gcn.nasa.gov/schema/stable/gcn/notices/icecube/LvkNuTrackSearch.schema.json"
+    collected_results["$schema"]= "https://gcn.nasa.gov/schema/v3.0.0/gcn/notices/icecube/lvk_nu_track_search.schema.json"
     collected_results["type"]= "IceCube LVK Alert Nu Track Search"
 
     eventtime = record.find('.//ISOTime').text
