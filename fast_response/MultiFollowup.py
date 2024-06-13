9a2b51ffa5dfd4d12f329c88fbb1e510d184c4f8
Christoph Raab <chraab@icecube.wisc.edu>
2024-06-13 13:07:12 -0500

    MultiFollowup: change to Greco with factor 2 fix

--- fast_response/MultiFollowup.py
+++ fast_response/MultiFollowup.py
@@ -3,15 +3,14 @@ from .FastResponseAnalysis_ifelse import PointSourceFollowup
 class ExternalFollowup(PointSourceFollowup):
     '''
     Class for external point-source or extended source followup.
-    By default, uses a fixed index of 2.0 in LLH. Based on 
-    the PointSourceFollowup class
+    By default, uses a fixed index of 2.5 in LLH. Based on 
+    the PointSourceFollowup class adapted to accept multiple samples
 
     '''
     _dataset = ["GFUOnline_v001p02",
-                "GrecoOnline_v002p10",
+                "GrecoOnline_v002pFactor",
                ]
     
     _fix_index = True
     _float_index = not _fix_index
     _index = 2.5
-    _scramble = True
