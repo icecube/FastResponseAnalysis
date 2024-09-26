5fd8447f615777d7c0258139fb666aeb8b9fff2a
Christoph Raab <chraab@icecube.wisc.edu>
2024-09-26 07:11:07 -0500

    with _inherit default, make class names consistent

--- fast_response/MultiExternalFollowup.py
+++ fast_response/MultiExternalFollowup.py
@@ -9,20 +9,25 @@ import numpy as np
 # instance: specific source/follow-up
 class GFUFollowup(PointSourceFollowup):
     _dataset = "GFUOnline_v001p02"
-    _season_names = [f"IC86, 201{y}" for y in range(1, 10)]
+    _season_names = [f"IC86, 201{y}" for y in range(7, 10)]
+    #_season_names = [f"IC86, 201{y}" for y in range(1, 10)]
     _floor = np.radians(0.2)
 
 class GrecoFollowup(PointSourceFollowup):
     _dataset = 'GrecoOnline_v002pFactor'
-    _season_names = [f"IC86, 20{y:02d}" for y in range(12, 22+1)]
+    _season_names = [f"IC86, 20{y:02d}" for y in range(18, 20+1)]
+    #_season_names = [f"IC86, 20{y:02d}" for y in range(12, 22+1)]
     _floor = np.radians(0.2) # can change this!
+    # extended/GRB-style LLH?
     
+#or any other number of definitions
 
 class MultiFollowup(MultiPointSourceFollowup):
     '''
     Class for external point-source or extended source followup.
-    By default, uses a fixed index of 2.5 in LLH. Based on 
+    By default, uses floating index of 2.5 in the LLH. Based on 
     the PointSourceFollowup class adapted to accept multiple samples
+    via the configuration of constituent follow-up analyses.
 
     '''
     _followups = [GFUFollowup, GrecoFollowup] # more consistent
