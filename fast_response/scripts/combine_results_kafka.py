6f483538e0b7da341b4762443261f2425bb377df
jessiethw <thwaites@wisc.edu>
2024-10-15 11:32:12 -0500

    add new id for v4.2.0 schema

--- fast_response/scripts/combine_results_kafka.py
+++ fast_response/scripts/combine_results_kafka.py
@@ -88,7 +88,9 @@ def SendTestAlert(results=None):
 def format_ontime_events_uml(events, event_mjd):
     ontime_events={}
     for event in events:
-        ontime_events[event['event']]={
+        unique_id = '{}_{}'.format(int(event['run']), int(event['event']))
+        ontime_events[unique_id]={
+            'id':[unique_id],
             'event_dt' : round((event['time']-event_mjd)*86400., 2),
             'localization':{
                 'ra' : round(np.rad2deg(event['ra']), 2),
@@ -100,15 +102,17 @@ def format_ontime_events_uml(events, event_mjd):
             #'event_pval_generic' : round(event['pvalue'],4)
         }
         if event['pvalue'] < 0.0001:
-            ontime_events[event['event']]['event_pval_generic'] = float('{:.1e}'.format(event['pvalue']))
+            ontime_events[unique_id]['event_pval_generic'] = float('{:.1e}'.format(event['pvalue']))
         else:
-            ontime_events[event['event']]['event_pval_generic'] = round(event['pvalue'],4)
+            ontime_events[unique_id]['event_pval_generic'] = round(event['pvalue'],4)
     return ontime_events
 
 def format_ontime_events_llama(events):
     ontime_events={}
     for event in events:
-        ontime_events[event['i3event']] = {
+        unique_id = '{}_{}'.format(int(event['i3run']), int(event['i3event']))
+        ontime_events[unique_id] = {
+            'id':[unique_id],
             'event_dt' : round(event['dt'],2),
             'localization':{
                 'ra' : round(event['ra'], 2),
@@ -120,9 +124,9 @@ def format_ontime_events_llama(events):
             #'event_pval_bayesian': round(event['p_value'],4)
         }
         if event['p_value'] < 0.0001:
-            ontime_events[event['i3event']]['event_pval_bayesian'] = float('{:.1e}'.format(event['p_value']))
+            ontime_events[unique_id]['event_pval_bayesian'] = float('{:.1e}'.format(event['p_value']))
         else:
-            ontime_events[event['i3event']]['event_pval_bayesian'] = round(event['p_value'],4)
+            ontime_events[unique_id]['event_pval_bayesian'] = round(event['p_value'],4)
     return ontime_events
 
 def combine_events(uml_ontime, llama_ontime):
@@ -196,7 +200,7 @@ def parse_notice(record, wait_for_llama=False, heartbeat=False):
         return
 
     collected_results = {}
-    collected_results["$schema"]= "https://gcn.nasa.gov/schema/v4.1.0/gcn/notices/icecube/lvk_nu_track_search.schema.json"
+    collected_results["$schema"]= "https://gcn.nasa.gov/schema/v4.2.0/gcn/notices/icecube/lvk_nu_track_search.schema.json"
     collected_results["type"]= "IceCube LVK Alert Nu Track Search"
 
     eventtime = record.find('.//ISOTime').text
@@ -500,7 +504,7 @@ def parse_notice(record, wait_for_llama=False, heartbeat=False):
                 if response.ok is True:
                     logger.info("GCN posted OK to {}".format(channel))
                 else:
-                    logger.info("Error posting skymap to {}!".format(channel))
+                    logger.info("Error posting Notice to {}!".format(channel))
         
             collected_results['subthreshold'] = subthreshold
             try:
@@ -518,22 +522,6 @@ def parse_notice(record, wait_for_llama=False, heartbeat=False):
                 logger.info('Sent alert to ROC for p<0.01')
             except:
                 logger.warning('Failed to send email/SMS notification.')
-
-            # try:
-            #     if params['Group'] == 'Burst': 
-            #         merger_type = 'Burst'
-            #     else:
-            #         k = ['BNS','NSBH','BBH']
-            #         probs = {j: float(params[j]) for j in k}
-            #         merger_type = max(zip(probs.values(), probs.keys()))[1]
-            # except:
-            #     logger.info('Could not determine type of event')
-            #     merger_type = None
-
-            # try:
-            #     subprocess.call(['/home/jthwaites/private/make_call.py', f'--type={merger_type}', '--call_anyway'])
-            # except Exception as e:
-            #     logger.warning('Call for p<0.01 failed.')
 
         else: 
             logger.info('p>0.01: no email/sms sent')
@@ -561,7 +549,7 @@ def parse_notice(record, wait_for_llama=False, heartbeat=False):
         if response.ok is True:
             logger.info("GCN posted OK to {}".format(channel))
         else:
-            logger.info("Error posting skymap to {}!".format(channel))
+            logger.info("Error posting Notice to {}!".format(channel))
 
 parser = argparse.ArgumentParser(description='Combine GW-Nu results')
 parser.add_argument('--run_live', action='store_true', default=False,
