7d5f880295a372a9b59399407a499248649d2a33
Christoph Raab <chraab@icecube.wisc.edu>
2026-06-12 11:33:31 -0500

    classes for DNN followup of GW

--- fast_response/MultiGWFollowup.py
+++ fast_response/MultiGWFollowup.py
@@ -7,9 +7,16 @@ import numpy as np
 
 class OnlyDNNOnlineFollowup(PriorFollowup):
     _base_dir = "/data/user/chraab/fast_response/multisample/variable_jitter_new_environment"
-    _sens_dir = join(_base_dir, "precomputed_background")
-    _bg_dir = join(_base_dir, "precomputed_background")
-    _dataset = "DNNCascadesOnline_v001p01"    
+    _sens_dir = join(_base_dir, "precomputed_sensitivity")
+    _bg_dir = join(_base_dir, "precomputed_trials")
+    _bg_format = '_'.join([
+    'precomputed_trials_delta_t_{delta_t:.2e}',
+    'nside_{nside}',
+    'index_{index}',
+    '{lookup}',
+    'low_stats.npz',
+    ])
+    _dataset = "DNNCascadesOnline_v001p01"
     _season_names = ["livestream"]
     _floor = np.radians(1.5) # can change this!
     _jitter = 3. # common default for DNN analyses
@@ -18,12 +25,20 @@ class OnlyDNNOnlineFollowup(PriorFollowup):
 
 class DNNOnlineFollowup(MultiPriorFollowup):
     _base_dir = "/data/user/chraab/fast_response/multisample/variable_jitter_new_environment"
-    _sens_dir = join(_base_dir, "precomputed_background")
-    _bg_dir = join(_base_dir, "precomputed_background")
+    _sens_dir = join(_base_dir, "precomputed_sensitivity")
+    _bg_dir = join(_base_dir, "precomputed_trials")
+    _bg_format = '_'.join([
+    'precomputed_trials_delta_t_{delta_t:.2e}',
+    'nside_{nside}',
+    'index_{index}',
+    '{lookup}',
+    'low_stats.npz',
+    ])
     _followups = [OnlyDNNOnlineFollowup]
     _fix_index = True # TODO check if this is what they do
     _float_index = not _fix_index
     _index = 2.0
+    _nside = 64
     
 class MultiGWFollowup(MultiPriorFollowup):
     _followups = [GFUFollowup, OnlyDNNOnlineFollowup]
